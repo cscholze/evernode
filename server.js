a955681b8c91736284dc33d57639cb967a827fff
cScholze <caleb.scholze@gmail.com>
2016-02-18 10:38:14 -0600

    added new note feture

--- server.js
+++ server.js
@@ -1,15 +1,35 @@
 'use strict';
 
+// DEPENDENCIES
+const bodyParser = require('body-parser');
 const express = require('express');
 
+// SERVER VARIABLES
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
+
+// MIDDLEWARE
+app.set('view engine', 'jade');
+
+app.use(bodyParser.urlencoded({extended: false}));
+
+// ROUTES
+app.get('/', (req, res, next) => {
   res.send('Server Running');
 });
 
+app.get('/notes/new', (req, res, next) => {
+    res.render('new-note');
+});
+app.post('/notes', (req, res, next) => {
+    console.log(req.body);
+    res.redirect('/');
+});
 
+
+
+// START SERVER
 app.listen(port, () => {
   console.log(`Evernode server running on port: ${port}`);
 });
